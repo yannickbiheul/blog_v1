bc2e4f9a112a6cb41b88c4095f20364d9becf0ca
Yannick Biheul <yannickbiheul@outlook.fr>
2021-04-07 20:22:50 +0200

    modifs viewPost

--- views/viewPost.php
+++ views/viewPost.php
@@ -5,12 +5,7 @@ $title = 'Deskad | ' . $post['title'];
 
 <div class="container">
 
-
-    <br>
-    
-    <h1 class="d-flex justify-content-center" style="margin-top:80px"><?= $post['title'] ?></h1>
-    <br>
-    <?php
+<?php
 if (isset($fails)) {
     foreach ($fails as $fail) {
         echo "<p style='color:red'>$fail</p>";
@@ -22,13 +17,20 @@ if (isset($fields)) {
 $date = new DateTime($post['creation_date']);
 $dateFR = $date->format('\L\e d-m-Y \à H\hi');
 ?>
-
+    <div class="d-flex flex-column align-items-center">
+        <h1 class="text-center" style="margin-top:80px"><?= $post['title'] ?></h1>
+    </div>
     <div class="row d-flex justify-content-center" style="padding:10px;">
-        <small class="text-muted"><?= $dateFR ?></small>
-        <h3><?= $post['resume'] ?></h3>
+        <div class="d-flex flex-column align-items-center">
+            <small class="text-muted"><?= $dateFR ?></small>
+            <h3 class="text-center"><?= $post['resume'] ?></h3>
+        </div>
+        
         <img class="rounded float-start" style="width: 40%;min-width:260px;margin:40px;" src="assets/images/<?= $post['image'] ?>" alt="<?= $post['image'] ?>">
         <br>
-        <div class="d-flex flex-column align-items-center"><?= nl2br($post['content']) ?></div>
+        <div class="d-flex flex-column align-items-center">
+            <?= nl2br($post['content']) ?>
+        </div>
     </div>
 
     <div class="row" style="margin:40px;" id="formComment">
